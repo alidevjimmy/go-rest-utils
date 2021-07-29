1e4bbfdd2ee7abe23ceae8849ef13ff9ebafca8e
alidevjimmy <hereisjimmymartin@gmail.com>
2021-07-29 10:50:29 +0430

    causes remove from code

--- rest_errors/rest_errors.go
+++ rest_errors/rest_errors.go
@@ -11,19 +11,17 @@ type RestErr interface {
 	Message() string
 	Status() int
 	Error() string
-	Causes() []interface{}
 }
 
 type restErr struct {
 	ErrMessage string        `json:"message"`
 	ErrStatus  int           `json:"status"`
 	ErrError   string        `json:"error"`
-	ErrCauses  []interface{} `json:"causes"`
 }
 
 func (e restErr) Error() string {
 	return fmt.Sprintf("message: %s - status: %d - error: %s - causes: %v",
-		e.ErrMessage, e.ErrStatus, e.ErrError, e.ErrCauses)
+		e.ErrMessage, e.ErrStatus, e.ErrError)
 }
 
 func (e restErr) Message() string {
@@ -34,16 +32,12 @@ func (e restErr) Status() int {
 	return e.ErrStatus
 }
 
-func (e restErr) Causes() []interface{} {
-	return e.ErrCauses
-}
 
-func NewRestError(message string, status int, err string, causes []interface{}) RestErr {
+func NewRestError(message string, status int, err string, ) RestErr {
 	return restErr{
 		ErrMessage: message,
 		ErrStatus:  status,
 		ErrError:   err,
-		ErrCauses:  causes,
 	}
 }
 
@@ -84,9 +78,6 @@ func NewInternalServerError(message string, err error) RestErr {
 		ErrMessage: message,
 		ErrStatus:  http.StatusInternalServerError,
 		ErrError:   "internal_server_error",
-	}
-	if err != nil {
-		result.ErrCauses = append(result.ErrCauses, err.Error())
 	}
 	return result
 }
